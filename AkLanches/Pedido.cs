3d461256932d2e5a5308ca50cef468a59925caf5
AndreikSarli <andreikelsarle@gmail.com>
2026-09-18 14:49:43 -0300

    feat: implementa logica classe pedido

--- AkLanches/Pedido.cs
+++ AkLanches/Pedido.cs
@@ -2,7 +2,17 @@
 {
     class Pedido
     {
-        public List<ItemCardapio> Itens { get; set; }
+        public List<ItemCardapio> Itens { get; set; } = new List<ItemCardapio>();
+
+        public double CalcularTotal()
+        {
+            double total = 0.0;
+            foreach (var item in Itens)
+            {
+                total += item.CalcularPrecoFinal();
+            }
+            return total;
+        }
 
 
     }
